1d6eddbddd572960e5b82205d6304bcd04b0574d
vinodkandukuri <120187566+RAJKUMARKANDUKURI@users.noreply.github.com>
2022-12-15 18:44:35 +0530

    vareables

---
+++ vareables.java
@@ -0,0 +1,23 @@
+package variables2;
+
+
+public class Vereables3 
+{
+String name="raju";
+static int a= 30;
+public void m1()
+{
+	System.out.println(a);
+}
+public static void m2()
+{
+	System.out.println("raju");
+	
+}
+public static void main(String[] args) 
+{
+	m2();
+	new Vereables3().m1();
+}
+
+}
