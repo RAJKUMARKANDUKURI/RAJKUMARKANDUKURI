95df414d7651a38fa7da70308dcb8ec63d556d58
vinodkandukuri <120187566+RAJKUMARKANDUKURI@users.noreply.github.com>
2022-12-17 18:14:44 +0530

    Create multiple for loops.java

---
+++ multiple for loops.java
@@ -0,0 +1,35 @@
+package instant;
+import java.util.Scanner;
+public class Test {
+	
+	int x = 0;
+	
+	
+	public static void main(String[] args) {
+//        Test ex = new Test();
+//        System.out.println(ex.x);
+//        int i =1;
+//        while(i<=10) {
+//        	System.out.println(i);
+//            i +=1; //i =i+1
+//        for(int i =50; i<= 100; i++) {
+//        	
+//        	System.out.println(i);
+            Scanner obj = new Scanner(System.in);
+            System.out.print("a: ");
+            String a = obj.nextLine();
+            
+            
+            System.out.print("b: ");
+            String b = obj.nextLine();
+            
+            System.out.println(a+b);
+//            for(int k=10;k>=0;k--)
+//            System.out.println(k);{
+//            	
+//        }
+	
+	}
+}
+
+
