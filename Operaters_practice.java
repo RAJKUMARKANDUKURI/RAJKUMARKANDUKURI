fc76265ecabc531a26c84569fb8483805566c0bd
vinodkandukuri <120187566+RAJKUMARKANDUKURI@users.noreply.github.com>
2023-01-21 16:05:53 +0530

    Create Operaters_practice.java

---
+++ Operaters_practice.java
@@ -0,0 +1,21 @@
+package java_practice;
+
+public class Operaters_practice {
+
+	public static void main(String[] args) {
+
+		int a = 20;
+		int b = 10;
+
+
+		System.out.println("addition is -->"+(a+b));
+		System.out.println("substraction is ==>"+(a-b));
+		System.out.println("multification is ==>"+(a*b));
+		System.out.println("division is ==>"+(a/b));
+		System.out.println("modulus is ==>"+(b%a));
+		System.out.println("Increment value is ==>"+(++a));
+		System.out.println("Decrement value is ==>"+(--b));
+
+	}
+
+}
