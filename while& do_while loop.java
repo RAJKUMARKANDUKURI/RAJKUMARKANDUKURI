342ae75fa7ac8618c1a95cec2b5ab25b3f57ec3a
vinodkandukuri <120187566+RAJKUMARKANDUKURI@users.noreply.github.com>
2023-01-22 18:04:28 +0530

    Create while& do_while loop.java

---
+++ while& do_while loop.java
@@ -0,0 +1,27 @@
+package loops;
+
+public class While_do_while {
+
+	public static void main(String[] args) {
+
+		int a =10;
+
+		while (a<100) {
+			a++;
+			System.out.println(a);
+		}
+
+		//do while loop
+
+
+		do {
+
+			System.out.println(a);
+			a++;
+		} while (a<500);
+	}}
+
+
+
+
+
